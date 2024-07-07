d7c4ee1577c2cb68645c5a85fecf21d7005a5e48
Peter Paravinja <peter.paravinja@optiweb.com>
2024-07-07 22:51:32 +0200

    day 1 part 1

--- 2019/day1.go
+++ 2019/day1.go
@@ -1,11 +1,27 @@
 package _2019
 
 import (
+	"fmt"
+	"math"
+	"strconv"
+
 	"github.com/oriiyx/aoc/internal/data"
 )
 
 func Part1() {
 	requestURL := "https://adventofcode.com/2019/day/1/input"
-	_ = data.GetInputArray(requestURL)
-	// fmt.Printf("%s", lines[0])
+	lines := data.GetInputArray(requestURL)
+
+	fuelReq := 0
+	for _, massString := range lines {
+		mass, _ := strconv.Atoi(massString)
+		fuelReq = fuelReq + calculateFuelRequirement(mass)
+	}
+
+	fmt.Println(fuelReq)
+}
+
+func calculateFuelRequirement(mass int) int {
+	fuelReq := math.Floor(float64(mass/3)) - 2
+	return int(fuelReq)
 }
