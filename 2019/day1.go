2bfb0346d63663542919f6c877302ddb0cd4203d
Peter Paravinja <peter.paravinja@optiweb.com>
2024-07-08 21:36:28 +0200

    part 2

--- 2019/day1.go
+++ 2019/day1.go
@@ -17,7 +17,7 @@ func Part1() {
 		fuelReq += calculateFuelRequirement(mass)
 	}
 
-	fmt.Printf("Day 1 fuel sum: %v", fuelReq)
+	fmt.Printf("Day 1 Part 1 fuel sum: %v \n", fuelReq)
 }
 
 func calculateFuelRequirement(mass int) int {
@@ -25,5 +25,27 @@ func calculateFuelRequirement(mass int) int {
 }
 
 func Part2() {
+	requestURL := "https://adventofcode.com/2019/day/1/input"
+	lines := data.GetInputArray(requestURL)
 
+	fuelReq := 0
+
+	for _, massString := range lines {
+		absoluteFuelMass := 0
+		mass, _ := strconv.Atoi(massString)
+		fuelReq += calculateAbsoluteFuelRequirement(mass, absoluteFuelMass)
+	}
+
+	fmt.Printf("Day 1 Part 2 fuel sum: %v \n", fuelReq)
+}
+
+func calculateAbsoluteFuelRequirement(mass int, absoluteMass int) int {
+	requirement := calculateFuelRequirement(mass)
+
+	if requirement > 0 {
+		absoluteMass += requirement
+		absoluteMass = calculateAbsoluteFuelRequirement(requirement, absoluteMass)
+	}
+
+	return absoluteMass
 }
--- main.go
+++ main.go
@@ -6,4 +6,5 @@ import (
 
 func main() {
 	_2019.Part1()
+	_2019.Part2()
 }
