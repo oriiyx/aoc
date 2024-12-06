09b5e94d6368a7c8faf9d085402a3596143cf121
oriiyx <paravinja92@gmail.com>
2024-12-06 18:56:22 +0100

    added timers

--- y24/day6_test.go
+++ y24/day6_test.go
@@ -1,11 +1,19 @@
 package y24
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func BenchmarkDay6Part1(b *testing.B) {
+	start := time.Now()
 	Day6Part1()
+	fmt.Printf("Part 1: %v\n", time.Since(start))
 }
 
 func BenchmarkDay6Part2(b *testing.B) {
+	start := time.Now()
 	Day6Part2()
+	fmt.Printf("Part 2: %v\n", time.Since(start))
 }
